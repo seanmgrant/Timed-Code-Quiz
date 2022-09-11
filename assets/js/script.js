be0322b3296116a05d1839e2a323fd43418d1262
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-09-10 19:02:32 -0600

    Added JS CSS HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,74 @@
 const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
+const landingPage = document.getElementById('landing')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
+const saveScoreEl = document.getElementById('save-score')
+const viewScores = document.getElementById('scores')
+const highScores = document.getElementById('high-scores')
 
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () =>{
-    currentQuestionIndex++
-    setNextQuestion()
-})
+// viewScores.addEventListener('click', showScores)
 
-function startGame() {
-    console.log('Started')
-    startButton.classList.add('hide')  
-    shuffledQuestions = question.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
+
+
+
+const questions = [
+    {
+      question: 'String values must be enclosed within _____ when being assigned to variables.',
+      answers: [
+        {text: '1. commas', correct: false},
+        {text: '2. curly brackets', correct: false},
+        {text: '3. quotes', correct: true},
+        {text: '4. parenthesis', correct: false}
+      ]
+    },
+    {
+      question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+      answers: [
+        {text: '1. JavaScript', correct: false},
+        {text: '2. terminal/bash', correct: false},
+        {text: '3. for loops', correct: false},
+        {text: '4. console.log', correct: true},
+      ]
+    },
+    {
+      question: 'Commonly used data types do NOT include:',
+      answers: [
+        {text: '1. strings', correct: false},
+        {text: '2. booleans', correct: false},
+        {text: '3. alerts', correct: true},
+        {text: '4. numbers', correct: true},
+      ]
+    },
+    {
+      question: 'The condition in an if/else statement is enclosed with ______.',
+      answers: [
+        {text: '1. quotes', correct: false},
+        {text: '2. curly brackets', correct: false},
+        {text: '3. parenthesis', correct: true},
+        {text: '4. square brackets', correct: true},
+      ]
+    }
+]
+
+function startGame(){
+    landingPage.classList.add('hide')
     questionContainerElement.classList.remove('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     setNextQuestion()
 }
 
-function setNextQuestion() {
-    // resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
+function showQuestion(question){
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
         const button = document.createElement('button')
         button.innerText = answer.text
         button.classList.add('btn')
-        if (answer.corrcet) {
+        if (answer.corrcet){
             button.dataset.correct = answer.correct
         }
         button.addEventListener('click', selectAnswer)
@@ -40,52 +76,51 @@ function showQuestion(question) {
     })  
 }
 
-function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
+function resetState(){
+    startButton.classList.add('hide')
+    while (answerButtonsElement.firstChild){
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
-    
 }
 
-function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct 
-    setStatusClass(document.body, correct )
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.corrcet)
-    })
-    if (shuffledQuestions.length . currentQuestionIndex + 1  ) {
-        nextButton.classList.remove('hide')
-    } else {
-        startButton.innerText = 'Restart'
-        startButton.classList.remove('hide')
-    }
+function setNextQuestion(){
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function setStatusClass(element,correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
-}
-
-function clearStatusClass(element) {
+function clearStatusClass(element){
     element.classList.remove('correct')
     element.classList.remove('wrong')
-    
 }
- 
-const question = [
-    {
-        question: 'your question here',
-        answers: [
-            { text: '4', correct: true },
-            { text: 22, correct: false} 
-        ]
 
+function setStatusClass(element, correct){
+    clearStatusClass(element)
+    if (correct){
+      element.classList.add('correct')
+    } else{
+      element.classList.add('wrong')
     }
-]
+}
+
+function selectAnswer(e){
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct 
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1  ){
+       currentQuestionIndex++
+       setNextQuestion()
+    } else{
+        questionContainerElement.classList.add('hide')
+        saveScoreEl.classList.remove('hide')
+    }
+}
+
+function showScores(){
+    questionContainerElement.classList.add('hide')
+    landingPage.classList.add('hide')
+    highScores.classList.remove('hide')
+  }
+ 
